641cff7ce53f1d87a983512d2a8d9aef6ec31688
Jere Stigell <jj.stigell@gmail.com>
2022-08-10 19:57:27 +0900

    10.15

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,7 @@
 import FormikTextInput from './FormikTextInput';
 import { Formik } from 'formik';
 import { Button, View, StyleSheet } from 'react-native';
+import { useNavigate } from 'react-router-native';
 import * as yup from 'yup';
 import useSignIn from '../hooks/useSignIn';
 
@@ -52,13 +53,16 @@ const SignInForm = ({ onSubmit }) => {
 const SignIn = () => {
 
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
     
     try {
       const { data } = await signIn({ username, password });
-      console.log(data);
+      if (data) {
+        navigate('/');
+      }
     } catch (e) {
       console.log(e);
     }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,12 +1,19 @@
 import { useMutation } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
+import useAuthStorage from './useAuthStorage';
+import { useApolloClient } from '@apollo/client';
 
 const useSignIn = () => {
   const [authenticate, result] = useMutation(AUTHENTICATE);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
     const response = await authenticate({ variables: { username, password } });
-    return { data: response.data.authenticate.accessToken };
+    const token = response.data.authenticate.accessToken;
+    await authStorage.setAccessToken(token);
+    apolloClient.resetStore();
+    return { data: token };
   };
 
   return [signIn, result];
